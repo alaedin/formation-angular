c3c9435eb0ec51f29b84e8642723da9805f9a1ab
alaeldin <arfaoui.ala.eddin@gmail.com>
2022-08-18 19:40:02 +0200

    use literal types on method signature

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FaceSnap} from "../model/FaceSnap";
+import {FaceSnapsService} from "../services/faceSnaps.service";
 
 @Component({
   selector: 'app-face-snap',
@@ -11,16 +12,19 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
   snapButton!: string;
 
+  constructor(private faceSnapService: FaceSnapsService) {
+  }
+
   ngOnInit() {
     this.snapButton = 'Oh Snap!';
   }
 
   onSnap() {
     if (this.snapButton === 'Oh Snap!') {
-      this.faceSnap.snap++;
+      this.faceSnapService.snapById(this.faceSnap.id,'snap');
       this.snapButton = 'Oops unSnap!'
     } else {
-      this.faceSnap.snap--;
+      this.faceSnapService.snapById(this.faceSnap.id,'unsnap');
       this.snapButton = 'Oh Snap!';
     }
   }
