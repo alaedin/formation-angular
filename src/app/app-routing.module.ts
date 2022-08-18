f485536b0bd89f56d4824a9f951157b591f9c62e
alaeldin <arfaoui.ala.eddin@gmail.com>
2022-08-18 20:11:22 +0200

    adding landing page coponent to navigate from home to snap-faces page adding routers adding routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {FaceSnapListComponent} from "./face-snap-list/face-snap-list.component";
+import {LandingPageComponent} from "./landing-page/landing-page.component";
 
 const routes: Routes = [
-  {path: 'face-snaps', component: FaceSnapListComponent}
+  {path: 'face-snaps', component: FaceSnapListComponent},
+  {path: '', component: LandingPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import {registerLocaleData} from "@angular/common";
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent,
     FaceSnapListComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
