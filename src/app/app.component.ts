32764dfc8a2be89a2f27d23ebc9d3649102297cd
alaeldin <arfaoui.ala.eddin@gmail.com>
2022-08-19 12:42:59 +0200

    use subscribe create many instance of the same observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,9 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit() {
+    const interval$ = interval(1000);
+    interval$.subscribe(value => console.log(value));
+    setTimeout(()=> interval$.subscribe(value => console.log(value)), 3000);
+
   }
 }
