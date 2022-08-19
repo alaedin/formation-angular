7f33b6b924e393b7d52a1542c43e431b5b5714cc
alaeldin <arfaoui.ala.eddin@gmail.com>
2022-08-19 12:57:29 +0200

    use map from (rxjs/operators) to browse an observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {interval, Observable} from "rxjs";
+import {map} from "rxjs/operators"
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,16 @@ import {interval, Observable} from "rxjs";
 })
 export class AppComponent implements OnInit {
 
-  interval$!: Observable<number>;
+  interval$!: Observable<string>;
 
 
   ngOnInit() {
-    this.interval$ = interval(1000);
+    this.interval$ = interval(1000).pipe(
+      map(value => value % 2 === 0 ?
+        `the number ${value} is pair` :
+        `the number ${value} is odd`
+        )
+    );
 
   }
 }
