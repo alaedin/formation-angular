fa978ce7cf7824ed8abcc45f0415380a837ae10e
alaeldin <arfaoui.ala.eddin@gmail.com>
2022-08-19 20:16:04 +0200

    use High level operators for observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {filter, interval, map, Observable, tap} from "rxjs";
+import {delay, interval, map, mergeMap, of, take, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -8,21 +8,30 @@ import {filter, interval, map, Observable, tap} from "rxjs";
 })
 export class AppComponent implements OnInit {
 
-  interval$!: Observable<string>;
-
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
   ngOnInit() {
-    this.interval$ = interval(1000).pipe(
-      filter(value => value % 3 === 0),
-      map(value => value % 2 === 0 ?
-        `the number ${value} is pair` :
-        `the number ${value} is odd`
-      ),
-      tap(value => this.logger(value)),
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      mergeMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({color, trainIndex}).pipe(
+      delay(isRedTrain ? 5000 : 6000)
     );
   }
 
-  logger(text: string) {
-    console.log('Log ' + text);
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
   }
 }
