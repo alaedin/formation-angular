47ca41882ed9a4a3cf37695452453cb5767e5cfb
alaeldin <arfaoui.ala.eddin@gmail.com>
2022-08-21 16:33:57 +0200

    create landing-page module move landing-page folder to landing-page/components/landing-page move HttpClientModule from app.module.ts to code.module.ts update import add LandingPageModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,22 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import {FormsModule} from "@angular/forms";
 import {CoreModule} from "./core/core.module";
 import {FaceSnapsModule} from "./face-snaps/face-snaps.module";
-import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {LandingPageComponent} from "./landing-page/components/landing-page/landing-page.component";
+import {LandingPageModule} from "./landing-page/landing-page.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     CoreModule,
     FaceSnapsModule,
-
+    LandingPageModule,
 
   ],
   providers: [],
--- src/app/landing-page/components/landing-page/landing-page.component.ts
+++ src/app/landing-page/components/landing-page/landing-page.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-import {FormsModule, NgForm} from "@angular/forms";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-landing-page',
